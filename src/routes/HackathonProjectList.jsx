99a8930fa48923fc2bfee32048a512ac617bf209
marekk3301 <55376943+marekk3301@users.noreply.github.com>
2025-04-30 07:45:11 +0200

    minor changes, rename projectList

---
+++ src/routes/HackathonProjectList.jsx
@@ -0,0 +1,38 @@
+import { Link } from "react-router-dom";
+
+import "../css/HackathonProjectList.css"
+
+import hackathons from '../assets/hackathons.json';
+
+const HackathonProjectList = () => {
+
+    const projectList = hackathons.map((hackathon) => {
+        if (hackathon.icon == "") {
+          hackathon.icon = "generic.svg"
+        }
+
+        if (hackathon.id) {
+          console.log(hackathon.event.name, hackathon.event.date);
+          return (
+            <div className="project__icon">
+              <Link to={"/project/" + hackathon.id}>
+                <img className="glitch" src={process.env.PUBLIC_URL + "/galleries/project_icons/" + hackathon.icon} alt={hackathon.id} />
+              </Link>
+            </div>
+          );
+        }   
+        return null;
+    });
+
+    return (
+      <div className="list__container">
+        <div className="list__mask">
+          <div className="project__list">
+            {projectList}
+          </div>
+        </div>
+      </div>
+    );
+  }
+ 
+  export default HackathonProjectList
